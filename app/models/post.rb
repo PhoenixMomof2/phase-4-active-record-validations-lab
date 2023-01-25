e69b89cc6754a6433b540554ae4d323b179b6f9a
PhoenixMomof2 <89948037+PhoenixMomof2@users.noreply.github.com>
2023-01-24 23:15:41 -0500

    commit tree 6cef6288f9c324ab3226ac71ed524ba820314e7b

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, { :length => { :minimum => 250 } }
+  validates :summary, { :length => { :maximum => 250 } }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :clickbait_y?
+
+  CLICKBAIT_PHRASES = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait_y?
+    if CLICKBAIT_PHRASES.none? { |phrase| phrase.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
 end
